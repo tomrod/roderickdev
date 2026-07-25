a190c505ca0c87d30d54aa0ddda4b41d8e0aa769
Thomas Roderick <tom@roderick.cc>
2026-07-25 12:53:48 -0400

    REAL IMPACT TIME (#15)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
+import remarkGfm from 'remark-gfm';
 
 export default defineConfig({
   site: 'https://www.roderick.dev',
   output: 'static',
   build: {
     assets: 'assets',
+  },
+  markdown: {
+    remarkPlugins: [remarkGfm],
   },
 });
