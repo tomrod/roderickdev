87dac253bf6cd95831f11b422959e26bf28e50fc
tomrod <tom@roderick.cc>
2026-07-29 21:51:36 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://www.roderick.dev',
   output: 'static',
+
   build: {
     assets: 'assets',
   },
+
+  integrations: [sitemap()],
 });
